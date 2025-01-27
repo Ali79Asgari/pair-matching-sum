65c2a034399d966f65c6190f4c7243f777aff108
Ali <ali79asgari@gmail.com>
2025-01-28 00:41:15 +0330

    feat: initialize project

---
+++ src/main/java/org/example/pairmatchingsum/PairMatchingSumApplication.java
@@ -0,0 +1,13 @@
+package org.example.pairmatchingsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairMatchingSumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PairMatchingSumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pairmatchingsum/PairMatchingSumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pairmatchingsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairMatchingSumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
